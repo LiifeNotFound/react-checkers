d234e21b7d212d24e17076c636222db21a55fede
liife:Windows-Lenovo50 <programexception@icloud.com>
2021-03-20 03:00:18 +0000

    Copyright and styles

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,30 +1,39 @@
 import "./table.css";
 import HorizontalSquares from "../HorizontalSquares/horizontalSquares";
 
-var x = 1;
-
 function App(props) {
 	return (
 		<>
 			<div className="table">
-				<HorizontalSquares {...props} pieceType="black-pieces" />
+				<HorizontalSquares
+					{...props}
+					pieceType="black-pieces"
+				/>
 				<HorizontalSquares
 					{...props}
 					pieceType="black-pieces"
 					isInverted="true"
 				/>
-				<HorizontalSquares {...props} pieceType="black-pieces" />
+				<HorizontalSquares
+					{...props}
+					pieceType="black-pieces"
+				/>
 				<HorizontalSquares
 					{...props}
 					isInverted="true"
 				/>
 				<HorizontalSquares {...props} />
-				<HorizontalSquares pieceType="white-pieces"
+				<HorizontalSquares
+					pieceType="white-pieces"
 					{...props}
 					isInverted="true"
 				/>
-				<HorizontalSquares pieceType="white-pieces" {...props} />
-				<HorizontalSquares pieceType="white-pieces"
+				<HorizontalSquares
+					pieceType="white-pieces"
+					{...props}
+				/>
+				<HorizontalSquares
+					pieceType="white-pieces"
 					{...props}
 					isInverted="true"
 				/>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Table from './components/Table/Table.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Table from "./components/Table/Table.jsx";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Table newGame />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<h1>
+			Checkers | Developed by{" "}
+			<a href="http://t04-hendrick.vigion.pt">
+				Hendrik Lelis
+			</a>{" "}
+			| Working on it{" "}
+      | Started at 20/3/2001
+		</h1>
+		<Table newGame />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
